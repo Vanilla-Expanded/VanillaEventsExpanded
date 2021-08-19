f2e659de196c8c7e37678fbd0fde8b45d4cf0423
Kylian <kylian.balde@gmail.com>
2021-08-19 18:34:03 +0200

    Fix Wanderer Join Traitor

---
+++ 1.3/Source/VEE/QuestNode_Root_WandererJoinTraitor_WalkIn.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using Verse;
+
+namespace VEE
+{
+    internal class QuestNode_Root_WandererJoinTraitor_WalkIn : QuestNode_Root_WandererJoin_WalkIn
+    {
+        public override Pawn GeneratePawn()
+        {
+            PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDefOf.Villager, Faction.OfPlayer, mustBeCapableOfViolence: true);
+            Pawn pawn = PawnGenerator.GeneratePawn(request);
+
+            pawn.health.AddHediff(VEE_DefOf.Traitor);
+
+            if (!pawn.IsWorldPawn())
+                Find.WorldPawns.PassToWorld(pawn);
+            return pawn;
+        }
+    }
+}
