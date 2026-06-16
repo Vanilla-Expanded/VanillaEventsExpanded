bbd2589ac7d34deab49ac03115e4aec9543c58cc
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-06-16 21:54:46 +0200

    Added deep snow

---
+++ 1.6/Source/VEE/Building and Thing/DeepSnow.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VEE
+{
+    public class DeepSnow : Mineable
+    {
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            try
+            {
+                WeatherBuildupUtility.AddSnowRadial(this.Position, Find.CurrentMap, 1f, 1f);
+            }
+            catch (Exception) { }
+
+
+            base.Destroy(mode);
+        }
+    }
+}
