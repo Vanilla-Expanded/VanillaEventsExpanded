f4ddf888eb5a8136a6326067dd70d4784c4d84ad
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-06-21 15:33:39 +0200

    Crashlanded

--- 1.6/Source/VEE/IncidentWorker/Crashlanded.cs
+++ 1.6/Source/VEE/IncidentWorker/Crashlanded.cs
@@ -1,6 +1,7 @@
 ﻿using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace VEE.RegularEvents
@@ -40,9 +41,32 @@ namespace VEE.RegularEvents
             for (int i = 0; i < 3; i++)
             {
                 Pawn pawn = ThingUtility.FindPawn(VEE_DefOf.VEE_CrashlandedColonists.root.Generate(pawnParms));
+                if (Rand.Chance(0.5f))
+                {
+                    pawn.health.AddHediff(HediffDefOf.CryptosleepSickness);
+                }
                 pawn.SetFaction(faction);
                 things.Add(pawn);
             }
+            PawnKindDef kindDef = PossiblePets().RandomElementByWeight((PawnKindDef td) => td.RaceProps.petness);
+            Pawn animal = PawnGenerator.GeneratePawn(kindDef, faction);
+            things.Add(animal);
+
+            Thing silver = ThingMaker.MakeThing(ThingDefOf.Silver);
+            silver.stackCount = 200;
+            things.Add(silver);
+
+            Thing meals = ThingMaker.MakeThing(ThingDefOf.MealSurvivalPack);
+            meals.stackCount = 20;
+            things.Add(meals);
+
+            Thing components = ThingMaker.MakeThing(ThingDefOf.ComponentIndustrial);
+            components.stackCount = 10;
+            things.Add(components);
+
+            Thing medicine = ThingMaker.MakeThing(ThingDefOf.MedicineIndustrial);
+            medicine.stackCount = 10;
+            things.Add(medicine);
 
             IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
             DropPodUtility.DropThingsNear(intVec, map, things, 110, canInstaDropDuringInit: false, leaveSlag: true);
@@ -50,6 +74,11 @@ namespace VEE.RegularEvents
             Find.LetterStack.ReceiveLetter(label, text, LetterDefOf.NeutralEvent, new TargetInfo(intVec, map, false), null, null);
             return true;
 
+        }
+
+        private IEnumerable<PawnKindDef> PossiblePets()
+        {
+            return DefDatabase<PawnKindDef>.AllDefs.Where((PawnKindDef td) => td.RaceProps.Animal && td.RaceProps.petness>0);
         }
     }
 }
