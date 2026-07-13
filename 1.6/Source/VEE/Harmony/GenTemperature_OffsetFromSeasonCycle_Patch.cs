5d3854985e150be0a07f77d4862afcaa4ab007c2
Taranchuk <nfhfyxer@gmail.com>
2026-07-14 00:14:50 +0300

    VEE_GenTemperature_OffsetFromSeasonCycle_Patch performance optimization patch

--- 1.6/Source/VEE/Harmony/GenTemperature_OffsetFromSeasonCycle_Patch.cs
+++ 1.6/Source/VEE/Harmony/GenTemperature_OffsetFromSeasonCycle_Patch.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
@@ -9,14 +8,26 @@ namespace VEE
     [HarmonyPatch(typeof(GenTemperature), "OffsetFromSeasonCycle")]
     public static class VEE_GenTemperature_OffsetFromSeasonCycle_Patch
     {
+        private static int lastOffsetCachedTick = -1;
+        private static float cachedOffset;
         public static void Postfix(ref float __result, PlanetTile tile)
         {
             if (tile.LayerDef == PlanetLayerDefOf.Surface)
             {
-                foreach (var cond in Find.World.gameConditionManager.ActiveConditions.OfType<GameCondition_MultiStage>())
+                var ticksGame = Find.TickManager.TicksGame;
+                if (lastOffsetCachedTick != ticksGame)
                 {
-                    __result += cond.TemperatureOffsetWorld();
+                    lastOffsetCachedTick = ticksGame;
+                    cachedOffset = 0f;
+                    foreach (var activeCondition in Find.World.gameConditionManager.ActiveConditions)
+                    {
+                        if (activeCondition is GameCondition_MultiStage cond)
+                        {
+                            cachedOffset += cond.TemperatureOffsetWorld();
+                        }
+                    }
                 }
+                __result += cachedOffset;
             }
         }
     }
